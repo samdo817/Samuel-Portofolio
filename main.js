e609783a479fe8c71e17d4ceb7b6b75f4403beb1
samdo817 <phantom72008@gmail.com>
2024-12-01 20:57:55 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    // Toggle nav list and burger class
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
